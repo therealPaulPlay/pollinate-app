cdc9e1d509ba6f9668b8da417bc8388803432df7
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-07-24 23:47:34 +0200

    feat: design improvements

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,19 @@
 <script>
 	import { page } from "$app/state";
 	import Button from "$lib/components/ui/button/button.svelte";
+	import { vibrate } from "$lib/utils/vibrate";
 	import { Home } from "lucide-svelte";
 </script>
 
 <div class="flex h-full flex-col items-center justify-center gap-4 text-center">
-	<h1 class="text-7xl font-bevellier">{page.status}</h1>
-	<p class="max-w-80">Error "{page.error.message}" occured. Thankfully, we're not allergic to errors.</p>
-    <Button class="fixed bottom-10" href="/"><Home /> Go home</Button>
+	<h1 class="font-bevellier text-7xl">{page.status}</h1>
+	<p class="max-w-70">Error: <i>{page.error.message}</i>. Thankfully, we're not allergic to errors.</p>
+	<Button
+		class="fixed bottom-12"
+		href="/"
+		size="lg"
+		onclick={() => {
+			vibrate.light();
+		}}><Home /> Go home</Button
+	>
 </div>
--- src/routes/onboarding/+page.svelte
+++ src/routes/onboarding/+page.svelte
@@ -5,13 +5,14 @@
 </script>
 
 <div class="flex h-full flex-col items-center">
-	<div class="mt-30 space-y-2 text-center">
+	<div class="mt-16 space-y-2 text-center">
+        <img alt="logo" src="/images/logo_500x500.png" class="w-32 h-32 mx-auto" />
 		<h1 class="font-bevellier text-7xl">Pollinate</h1>
 		<p>No-nonsense pollen forecast.</p>
 	</div>
-	<div class="mt-auto mb-5 max-w-md space-y-6 text-center">
+	<div class="mt-auto mb-8 max-w-md space-y-6 text-center">
 		<p class="text-sm max-w-4/5 mx-auto">
-			By clicking on "Continue", you agree to our <a href="/privacy" class="font-semibold hover:underline"
+			By clicking on <i>Get started</i>, you agree to our <a href="/privacy" class="font-semibold hover:underline"
 				>privacy policy</a
 			>
 			and our <a href="/tems" class="font-semibold hover:underline">terms of use</a>.
@@ -20,7 +21,7 @@
 			size="lg"
 			onclick={() => {
 				vibrate.light();
-			}}>Continue <ArrowRight /></Button
+			}}>Get started <ArrowRight /></Button
 		>
 	</div>
 </div>
