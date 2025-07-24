abe1e21c7baa21a23a5232040628140d1fb4447d
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-07-25 00:02:16 +0200

    style: shift logo down

--- src/routes/onboarding/+page.svelte
+++ src/routes/onboarding/+page.svelte
@@ -5,13 +5,13 @@
 </script>
 
 <div class="flex h-full flex-col items-center">
-	<div class="mt-16 space-y-2 text-center">
+	<div class="mt-[calc(55dvh-225px)] space-y-2 text-center">
         <img alt="logo" src="/images/logo_500x500.png" class="w-32 h-32 mx-auto" />
 		<h1 class="font-bevellier text-7xl">Pollinate</h1>
-		<p>No-nonsense pollen forecast.</p>
+		<p class="-mt-1">No-nonsense pollen forecast.</p>
 	</div>
 	<div class="mt-auto mb-8 max-w-md space-y-6 text-center">
-		<p class="text-sm max-w-4/5 mx-auto">
+		<p class="text-sm max-w-4/5 mx-auto opacity-75">
 			By clicking on <i>Get started</i>, you agree to our <a href="/privacy" class="font-semibold hover:underline"
 				>privacy policy</a
 			>
