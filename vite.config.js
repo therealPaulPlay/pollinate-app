f8685f05d09f594f4c09ca01b53f94023c0529b4
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-07-31 21:26:07 +0200

    feat: add refined icons, switch from cookie-based to localstorage-based paraglidejs strategy

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
-			strategy: ['cookie', 'preferredLanguage', 'baseLocale'],
+			strategy: ['localStorage', 'preferredLanguage', 'baseLocale'],
 			prefixDefaultLanguage: "always", // This forces cookie to be respected!
 		}),
 		tailwindcss()
